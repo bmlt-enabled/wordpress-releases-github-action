3af8ed5d891ea2839024856add87e89f4621bc53
Patrick Joyce <pjaudiomv@gmail.com>
2022-09-28 09:21:22 -0400

    cleanup

--- src/update_release.py
+++ src/update_release.py
@@ -6,9 +6,6 @@ import boto3
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
 
-account = boto3.client('sts').get_caller_identity().get('Account')
-
-
 def get_hash(file):
     '''
     Return sha256 hash of file
@@ -20,7 +17,6 @@ def get_hash(file):
     return sha256_hash.hexdigest()
 
 
-id = os.environ.get("INPUT_ID")
 file = os.environ.get("INPUT_FILE")
 s3_key = os.environ.get("INPUT_S3_KEY")
 deployment_type = os.environ.get("INPUT_DEPLOYMENNT_TYPE")
@@ -33,19 +29,16 @@ credentials = session.get_credentials()
 creds = credentials.get_frozen_credentials()
 
 data = {
-    "id": id,
+    "id": s3_key,
     "filename": file_name,
     "sha256": file_sha,
-    "deploymentType": "wordpress"
+    "deploymentType": deployment_type
 }
 print(data)
 headers = {'Content-Type': 'application/x-amz-json-1.1'}
 request = AWSRequest(method='PUT', url=url, data=json.dumps(data), headers=headers)
 SigV4Auth(creds, "execute-api", 'us-east-1').add_auth(request)
 response = requests.request(method='PUT', url=url, headers=dict(request.headers), data=json.dumps(data))
-print(response.text)
-print(response.status_code)
 
-print(f"::set-output name=set_id::{id}")
-print(f"::set-output name=file_sha::{file_sha}")
-print(f"::set-output name=account::{response.status_code}")
+print(f"::set-output name=put_data::{json.dumps(data)}")
+print(f"::set-output name=status_code::{response.status_code}")
