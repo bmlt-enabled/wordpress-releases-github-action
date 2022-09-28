9a8f0ab6cb33333726fd095a56193b20d47d5bed
Patrick Joyce <pjaudiomv@gmail.com>
2022-09-27 22:52:19 -0400

    test sha

--- src/update_release.py
+++ src/update_release.py
@@ -1,9 +1,25 @@
 import os
+import hashlib
+
+
+def get_hash(file):
+    '''
+    Return sha256 hash of file
+    '''
+    sha256_hash = hashlib.sha256()
+    with open(file,"rb") as f:
+        for byte_block in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(byte_block)
+    return sha256_hash.hexdigest()
+
 
 id = os.environ.get("INPUT_ID")
+file = os.environ.get("INPUT_FILE")
+file_sha = get_hash(file)
 if id:
     name = id + " yolo"
 else:
     name = id
 
 print(f"::set-output name=set_id::{name}")
+print(f"::set-output name=file_sha::{file_sha}")
