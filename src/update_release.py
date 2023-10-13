bcd2d735d6b9722880574ccf54f58bcfe4f84811
Patrick Joyce <pjaudiomv@gmail.com>
2023-10-12 23:53:43 -0400

    fix typo, cleanup

--- src/update_release.py
+++ src/update_release.py
@@ -6,53 +6,77 @@ import boto3
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
 
+CHUNK_SIZE = 4096
+DEFAULT_DEPLOYMENT_TYPE = "wordpress"
+CONTENT_TYPE = 'application/x-amz-json-1.1'
+REGION = 'us-east-1'
+
+
 def get_hash(file):
     '''
-    Return sha256 hash of file
+    Return sha256 hash of file.
     '''
     sha256_hash = hashlib.sha256()
-    with open(file,"rb") as f:
-        for byte_block in iter(lambda: f.read(4096),b""):
+    with open(file, "rb") as f:
+        for byte_block in iter(lambda: f.read(CHUNK_SIZE), b""):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
-def set_action_output(output_name, value) :
-    """Sets the GitHub Action output, with backwards compatibility for
-    self-hosted runners without a GITHUB_OUTPUT environment file.
 
-    Keyword arguments:
-    output_name - The name of the output
-    value - The value of the output
-    """
-    if "GITHUB_OUTPUT" in os.environ :
-        with open(os.environ["GITHUB_OUTPUT"], "a") as f :
+"""Sets the GitHub Action output, with backwards compatibility for
+self-hosted runners without a GITHUB_OUTPUT environment file.
+
+Keyword arguments:
+output_name - The name of the output
+value - The value of the output
+"""
+def set_action_output(output_name, value):
+    if "GITHUB_OUTPUT" in os.environ:
+        with open(os.environ["GITHUB_OUTPUT"], "a") as f:
             print("{0}={1}".format(output_name, value), file=f)
-    else :
+    else:
         print("::set-output name={0}::{1}".format(output_name, value))
 
 
-file = os.environ.get("INPUT_FILE")
-s3_key = os.environ.get("INPUT_S3_KEY")
-deployment_type = os.environ.get("INPUT_DEPLOYMENNT_TYPE") if os.environ.get("INPUT_DEPLOYMENNT_TYPE") else "wordpress"
-file_sha = get_hash(file)
-file_name = file.rsplit('/', 1)[-1]
-url = f"https://release-api.aws.bmlt.app/releases/{s3_key}"
+"""Uploads release data to s3.
 
-session = boto3.Session()
-credentials = session.get_credentials()
-creds = credentials.get_frozen_credentials()
+Keyword arguments:
+file - The file to upload
+s3_key - The key in s3 to upload to
+deployment_type - Deployment type
+"""
+def upload_to_s3(file, s3_key, deployment_type):
+    file_sha = get_hash(file)
+    file_name = file.rsplit('/', 1)[-1]
+    url = f"https://release-api.aws.bmlt.app/releases/{s3_key}"
 
-data = {
-    "id": s3_key,
-    "filename": file_name,
-    "sha256": file_sha,
-    "deploymentType": deployment_type
-}
-print(data)
-headers = {'Content-Type': 'application/x-amz-json-1.1'}
-request = AWSRequest(method='PUT', url=url, data=json.dumps(data), headers=headers)
-SigV4Auth(creds, "execute-api", 'us-east-1').add_auth(request)
-response = requests.request(method='PUT', url=url, headers=dict(request.headers), data=json.dumps(data))
+    session = boto3.Session()
+    credentials = session.get_credentials()
+    creds = credentials.get_frozen_credentials()
 
-set_action_output("put_data", json.dumps(data))
-set_action_output("status_code", response.status_code)
+    data = {
+        "id": s3_key,
+        "filename": file_name,
+        "sha256": file_sha,
+        "deploymentType": deployment_type
+    }
+    print(data)
+
+    headers = {'Content-Type': CONTENT_TYPE}
+    request = AWSRequest(method='PUT', url=url, data=json.dumps(data), headers=headers)
+    SigV4Auth(creds, "execute-api", REGION).add_auth(request)
+    response = requests.request(method='PUT', url=url, headers=dict(request.headers), data=json.dumps(data))
+
+    set_action_output("put_data", json.dumps(data))
+    set_action_output("status_code", response.status_code)
+
+if __name__ == "__main__":
+    file = os.environ.get("INPUT_FILE")
+    s3_key = os.environ.get("INPUT_S3_KEY")
+    deployment_type = os.environ.get("INPUT_DEPLOYMENT_TYPE", DEFAULT_DEPLOYMENT_TYPE)
+
+    try:
+        upload_to_s3(file, s3_key, deployment_type)
+    except Exception as e:
+        print(f"Error: {e}")
+        set_action_output("error", str(e))
